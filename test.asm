a4f7a7ce723ae241895ea34b43efc856e48f0398
Piotr Fusik <fox@scene.pl>
2021-03-29 19:52:28 +0200

    Optional fast multiplication using a square lookup table.

--- test.asm
+++ test.asm
@@ -1,13 +1,19 @@
+unshrinkler_FAST	equ	1
 unshrinkler_PARITY	equ	0
-unshrinkler_data	equ	$3000
+unshrinkler_data	equ	$2200
 unshrinkler_zp	equ	$80
 
-	org	$3800
+	org	$3000
 main
+	lda	20
+	cmp:req	20
+	mwa	#0	19
 	mwa	#dl	$230
 	mwa	#packed_data_addr	unshrinkler_zp
 	mwa	#unpacked_addr	unshrinkler_zp+2
 	jsr	unshrinkler
+	lda	20
+	ldx	19
 	jmp	*
 	icl	'unShrinkler.asm'
 
